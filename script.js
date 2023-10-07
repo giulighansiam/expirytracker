a1d02c5efb789e506a98d40ad75c97edf267d220
giulighansiam <55515705+giulighansiam@users.noreply.github.com>
2023-10-07 02:14:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-  //var julian[][]
+ //var julian[][]
  
  function vitreset(){
 	document.getElementById("vit_form").reset();
+	document.getElementById("result").innerText="";
  }
  /*
  vitcalc()
@@ -12,13 +13,10 @@
  */
  function vitcalc(){
 	var expdate = new Date(document.getElementById("vit_expdate").value);
-	
 	var amt = document.getElementById("vit_pills").value/document.getElementById("vit_perday").value;
-	var newdate = new Date();
-	var oldtime = expdate.getTime();
-	var amttime = amt*86400;
-	newdate.setTime(oldtime-amtTime);
-	//document.getElementById("result").innerText="YYYY-MM-DD: "+expdate.getUTCFullYear()+"-"+(expdate.getUTCMonth()+1)+"-"+expdate.getUTCDate();
+	
+	var amttime = amt*86400000;
+	var newdate = new Date(expdate.getTime()-amttime);
 	document.getElementById("result").innerText="YYYY-MM-DD: "+newdate.getUTCFullYear()+"-"+(newdate.getUTCMonth()+1)+"-"+newdate.getUTCDate();
 	//note javascript dates are 1-31 for days but 0-11 for months
 	//why
