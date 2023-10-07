41a86949a966e24484094a221b6efdaa2315d55e
giulighansiam <55515705+giulighansiam@users.noreply.github.com>
2023-10-07 01:37:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- //var julian[][]
+  //var julian[][]
  
  function vitreset(){
 	document.getElementById("vit_form").reset();
@@ -15,9 +15,11 @@
 	
 	var amt = document.getElementById("vit_pills").value/document.getElementById("vit_perday").value;
 	var newdate = new Date();
-	newdate.setDate(expdate.getDate()-amt);
-	
-	document.getElementById("result").innerHTML="YYYY-MM-DD: "+newdate.getUTCFullYear()+"-"+(newdate.getUTCMonth()+1)+"-"+newdate.getUTCDate();
+	var oldtime = expdate.getTime();
+	var amttime = amt*86400;
+	newdate.setTime(oldtime-amtTime);
+	//document.getElementById("result").innerText="YYYY-MM-DD: "+expdate.getUTCFullYear()+"-"+(expdate.getUTCMonth()+1)+"-"+expdate.getUTCDate();
+	document.getElementById("result").innerText="YYYY-MM-DD: "+newdate.getUTCFullYear()+"-"+(newdate.getUTCMonth()+1)+"-"+newdate.getUTCDate();
 	//note javascript dates are 1-31 for days but 0-11 for months
 	//why
 	
